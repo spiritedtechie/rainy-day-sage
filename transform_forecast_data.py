e18954b5494949a56fce80aa0db462359bc1f7ea
Vijay Patel <spiritedtechie@gmail.com>
2023-08-17 12:52:20 +0100

    refactor: extracts a code mappings function

--- transform_forecast_data.py
+++ transform_forecast_data.py
@@ -1,45 +1,36 @@
-import json
 from datetime import datetime, timedelta
 
 
-# Weather forecast for each datetime is an object list that looks like:
+# Returns the weather forecast for each datetime is an object list that looks like:
 # [
 #   {"Datetime": "2023-08-16 09:00:00", "Wind Gust (mph)": "27", "Temperature (C)": "25", ... },
 #   {...}
 # ]
 def transform_data(data):
-
-    # An accessible data structure to lookup fullname & units for a forecast variable's code
-    codeMappings = {}
-    for code in data["SiteRep"]["Wx"]["Param"]:
-        code_name = code["name"]
-        codeMappings[code_name] = {
-            "units": code["units"],
-            "full_name": code["$"],
-            "column_name": f"""{code["$"]} ({code["units"]})""",
-        }
+    codeMappings = _forecastPropertyCodeMappings(data)
 
     object_keys = []
     object_list = []
+
     object_keys.append("Datetime")
     for i in codeMappings.values():
-        object_keys.append(i["column_name"])
+        object_keys.append(i["name"])
 
     for period in data["SiteRep"]["DV"]["Location"]["Period"]:
         dateText = period["value"]
 
-        threeHourlyChunk = period["Rep"]
-        for chunk in threeHourlyChunk:
-            minutesFromMidnight = int(chunk["$"])
+        threeHourlyBlock = period["Rep"]
+        for block in threeHourlyBlock:
+            minutesFromMidnight = int(block["$"])
             dateTime = datetime.strptime(dateText, "%Y-%m-%dZ")
             dateTime = dateTime + timedelta(minutes=minutesFromMidnight)
 
             forecastObj = {}
             forecastObj["Datetime"] = dateTime.strftime("%Y-%m-%d %H:%M:%S")
 
-            for key in chunk.keys():
+            for key in block.keys():
                 if key != "$":
-                    forecastObj[codeMappings[key]["column_name"]] = chunk[key]
+                    forecastObj[codeMappings[key]["name"]] = block[key]
 
             object_list.append(forecastObj)
 
@@ -47,3 +38,20 @@ def transform_data(data):
     # print(object_list)
 
     return object_list, object_keys
+
+
+# Creates an accessible data structure to lookup label for a forecast property code i.e.
+#
+# {
+#   "G": "Wind Gust (mph)"
+#   ...
+# }
+#
+def _forecastPropertyCodeMappings(data):
+    codeMappings = {}
+    for code in data["SiteRep"]["Wx"]["Param"]:
+        code_name = code["name"]
+        codeMappings[code_name] = {
+            "name": f"""{code["$"]} ({code["units"]})""",
+        }
+    return codeMappings
