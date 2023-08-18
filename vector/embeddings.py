4e011b4203cde9f34776c740a8a5075422819ed7
Vijay Patel <spiritedtechie@gmail.com>
2023-08-18 23:42:51 +0100

    fix: comments cached embedder as writing to filesystem blows vercel's filesystem limits

--- vector/embeddings.py
+++ vector/embeddings.py
@@ -12,11 +12,13 @@ open_ai_api_key = os.getenv("OPENAI_API_KEY")
 embeddings = OpenAIEmbeddings(
     model="text-embedding-ada-002", openai_api_key=open_ai_api_key
 )
-fs = LocalFileStore("./cache/")
-cached_embedder = CacheBackedEmbeddings.from_bytes_store(
-    embeddings, fs, namespace=embeddings.model
-)
+
+# Commenting - as blowing up the Vercel serverless function limit
+# fs = LocalFileStore("./cache/")
+# cached_embedder = CacheBackedEmbeddings.from_bytes_store(
+#     embeddings, fs, namespace=embeddings.model
+# )
 
 
 def get_embeddings():
-    return cached_embedder
+    return embeddings
