9f04b30eab44ae591fd2c435674ad1f068050625
Vijay Patel <spiritedtechie@gmail.com>
2023-08-19 13:35:52 +0100

    feat: adds application (nextjs and trpc) scaffold with a basic hello world response for now

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import Image from 'next/image'
 import { trpc } from "@/utils/trpc";
 
 export default function Home() {
   let { data: message, isLoading, isFetching } = trpc.hello.useQuery();
 
-  if (isLoading || isFetching) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="shadow-xl">{message}</div>
+        <div className="shadow-xl">{(isLoading || isFetching) ? "Loading..." : message}</div>
       </div>
     </main>
   )
