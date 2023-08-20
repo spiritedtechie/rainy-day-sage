a866534c83659faf461f74c0e2eff1b7d82f7511
Vijay Patel <spiritedtechie@gmail.com>
2023-08-20 11:25:45 +0100

    feat: includes forecast summary and responsive styles the two boxes

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,17 +1,40 @@
 import { appRouter } from "./api/trpc/router";
+import Image from "next/image";
 
 /** This is a React Server Component */
 export default async function RSCPage() {
   const caller = appRouter.createCaller({});
   // call the tRPC endpoint
-  const data = await caller.forecast.get();
+  const forecast = await caller.forecast.get();
 
   // we render this output on the server
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-400 from-green-500 p-10">
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-400 from-green-500 p-5">
       <div className="w-max">
-        <div className="overflow-hidden whitespace-normal text-3xl text-white font-bold">
-          {data["inspiring-message"]}
+        <div className="md:flex rounded-xl shadow-lg border border-gray-500 p-10 mt-10">
+          <img
+            src="/weatherperson.png"
+            alt="Weather person"
+            className="w-20 h-20 mx-auto md:mr-10"
+          />
+          <blockquote className="mt-5 md:mt-0">
+            <p className="text-lg text-white font-medium font-bold">
+              "{forecast.summary}"
+            </p>
+          </blockquote>
+        </div>
+
+        <div className="md:flex rounded-xl shadow-lg border border-gray-500 p-10 mt-10">
+          <img
+            src="/yoga.png"
+            alt="Yoga person"
+            className="w-20 h-20 mx-auto md:mr-10"
+          />
+          <blockquote className="mt-5 md:mt-0">
+            <p className="text-lg text-white font-medium font-bold">
+              "{forecast["inspiring-message"]}"
+            </p>
+          </blockquote>
         </div>
       </div>
     </main>
