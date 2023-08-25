b7576516660df31f032a400768c36f00add446e8
Vijay Patel <spiritedtechie@gmail.com>
2023-08-25 12:23:58 +0100

    experiment: expands the experiment to try transform on a custom (made-up) domain.

--- api/experiments/llm_transform.py
+++ api/experiments/llm_transform.py
@@ -23,24 +23,59 @@ log = get_logger()
 
 open_ai_api_key = os.getenv("OPENAI_API_KEY")
 
-template = """
-Each rep has a '$' field which represent the "minutes from midnight" from the period date.
+forecast_template = """
+Here is some JSON data.
+Each Rep becomes a row.
+Each Rep has a '$' field which represents the "minutes from midnight" from the period date.
+You have to calculate the actual date-time using the Period date and "minutes from midnight".
 For example, if the period date is 2023-07-10, and the $ value is 540, this represents 2023-07-10 09:00:00.
 ---------
 {json}
 ---------
 Include the CSV on a single line.
-Include the header row with field names and units.
+Include the header row with all field names and units.
+Include the calculated DateTime for each row.
+"""
+
+
+custom_domain_template = """
+Here is some JSON data.
+Each Block becomes a row.
+Each Block field code can be mapped to a meaningful label in the Dict Map.
+Each Block has a 'tm' field which represents the "minutes from midnight" from the Segment date.
+You have to calculate the actual date-time using the Segment date and "minutes from midnight".
+For example, if the Segment date is 2023-07-10, and the 'tm' value is 540, this represents 2023-07-10 09:00:00.
+---------
+{json}
+---------
+Include the CSV on a single line.
+Include the header row with all field names.
 Include the calculated DateTime for each row.
 """
 
 question = """
-Convert the data to CSV format.
+Convert the data to CSV format. 
 """
+
+test_data_sets = {
+    "weather_forecast": {
+        "file_path": "experiments/sample_forecast_data_slim.json",
+        "prompt_template": forecast_template,
+    },
+    "custom_domain": {
+        "file_path": "experiments/sample_data_madeup.json",
+        "prompt_template": custom_domain_template,
+    },
+}
+
+active_data_set = "custom_domain"  # Change me
+active_prompt_template = test_data_sets[active_data_set]["prompt_template"]
+active_data_file = test_data_sets[active_data_set]["file_path"]
+
 
 chat_prompt = ChatPromptTemplate(
     messages=[
-        SystemMessagePromptTemplate.from_template(template),
+        SystemMessagePromptTemplate.from_template(active_prompt_template),
     ],
     input_variables=["json"],
 )
@@ -55,16 +90,15 @@ llm = ChatOpenAI(
 chain = LLMChain(llm=llm, prompt=chat_prompt, output_key="result", verbose=True)
 
 
-# Read the sample JSON
-with open("data/met_office/sample_forecast_data_slim.json") as file:
+with open(active_data_file) as file:
     file_contents = file.read()
-    json = json.loads(file_contents)
+    json_obj = json.loads(file_contents)
 
 # Execute LLM chain
 with get_openai_callback() as cb:
     response = chain(
         {
-            "json": json,
+            "json": json_obj,
         },
         return_only_outputs=True,
     )
